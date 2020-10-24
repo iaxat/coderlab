6a408fba97683988e836a63491913cce927e9a12
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-24 15:25:52 -0600

    up

--- codewars/descendingorder.py
+++ codewars/descendingorder.py
@@ -2,9 +2,7 @@ def descending_order():
     n = int(input('Enter the number: '))
     lst = list(str(n))
     print('The number is: ', lst)
-    
     lst_new = lst.sort()
-    
     print(lst_new)
     
 
