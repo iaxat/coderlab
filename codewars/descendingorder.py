ca52735433d4e1a7c16faf6977916c0d987822be
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-24 15:56:54 -0600

    up

--- codewars/descendingorder.py
+++ codewars/descendingorder.py
@@ -1,13 +1,15 @@
 def descending_order():
+    
     n = int(input('Enter the number: '))
     lst = list(str(n))
+    
     lst_int = []
     lst_new = []
+
     for i in range(0,len(lst)):
         lst_int.append(int(lst[i]))
     # print('lst',lst)
     # print('lst_int',lst_int)
-
     lst_new = lst_int.sort(reverse=True)
     print(lst_new)
     
