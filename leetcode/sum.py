07e202cbfe866c8e86987b689d1c5c0a53c0b1bd
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-23 04:25:18 -0700

    up

--- leetcode/sum.py
+++ leetcode/sum.py
@@ -3,3 +3,11 @@
 # The program should take the input of list and target
 # The program should return the index values of the list which in sum will be equal Target
 
+def two_sum(list, target):
+    print("The Sum Problem")
+
+
+if __name__ == "__main__":
+    list = int(input("Enter the numbers"))
+    target = int(input("Enter Target Value"))
+    two_sum(list, target)
