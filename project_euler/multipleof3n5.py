f7a33ce0dfd3ffd9333dea759b12e7b8e2af5b01
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-29 06:06:33 -0700

    up

--- project_euler/multipleof3n5.py
+++ project_euler/multipleof3n5.py
@@ -2,8 +2,17 @@
 # Multiples of 3 and 5
 
 def multiples():
+    mul_array = []
+    sum = 0
     n = int(input("Enter the Number"))
+    for i in range (0,n+1):
+        if i%3 == 0 or i%5:
+            mul_array.append(i)
+            sum += i
+        else:
+            continue
     
+    print('Sum is the',sum)
 
 
 if __name__ == "__main__":
