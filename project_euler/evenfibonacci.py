69c1aeddb1922041cae392de6af7f50da28c0e20
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-29 18:18:24 -0700

    up

--- project_euler/evenfibonacci.py
+++ project_euler/evenfibonacci.py
@@ -1,7 +1,12 @@
 # Even Fibonacci Problems
 
 def even():
+    data = int(input('Enter the Value'))
     print('The Even Fibonacci Problem')
+    series_f = [1,2]
+    for data in data:
+        
+
     
 
 
