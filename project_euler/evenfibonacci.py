38ad66188f33cb1214a41d05c33de35569282124
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-29 19:59:45 -0700

    up

--- project_euler/evenfibonacci.py
+++ project_euler/evenfibonacci.py
@@ -5,7 +5,7 @@ def fib():
     number = int(input('Enter Number: '))
     i = number
     arr_fib = [1,2]
-    while i>= number-2:
+    while i >= number-2:
         sum = sum(arr_fib)
         arr_fib.append(sum)
     print(arr_fib)
