0509736e60fc47c7375f27852664f1cef313bb7b
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-05 08:07:15 -0700

    up

--- project_euler/evenfibonacci.py
+++ project_euler/evenfibonacci.py
@@ -3,9 +3,16 @@
 def even_fib():
     counter = 0
     number = int(input('The number for Fibonacci series'))
+    n1,n2 = 1,2
+
+    while counter<=number:
+        new = n1 + n2
+        n1 = n2
+        n2 = new
+
+        counter += 1
     
-
-
+    
 # main function
 if __name__ == "__main__":
     even_fib()
