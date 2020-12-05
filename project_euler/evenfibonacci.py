97ead83317e68340f4e6f8557ef10cbc62b97a5f
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-05 10:03:14 -0700

    up

--- project_euler/evenfibonacci.py
+++ project_euler/evenfibonacci.py
@@ -27,6 +27,7 @@ def even_fib():
 def reverse_fib():
     sum = 4000000
     new = 0
+    add = 0
     n1,n2 = 1,2
     rev_fib_arr = []
 
@@ -38,7 +39,12 @@ def reverse_fib():
     
     print(rev_fib_arr)
 
+    for i in rev_fib_arr:
+        if i%2 == 0:
+            add += i
     
+    print(add)
+
 
 
 
