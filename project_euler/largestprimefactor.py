eae2c48d089013e9635c75e818188ae876e67736
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-06 15:11:09 -0700

    up

--- project_euler/largestprimefactor.py
+++ project_euler/largestprimefactor.py
@@ -1,15 +1,3 @@
-# Largest Prime Factor
-
-# def prime_factor(n):
-#     data = int(input('Enter the Number: '))
-
-
-
-# if __name__ == "__main__":
-#     prime_factor(n=int(input('Enter the Number')))
-
-
-
 # Function definition to get all prime factors
 def get_prime_factors(n):
     i = 2
