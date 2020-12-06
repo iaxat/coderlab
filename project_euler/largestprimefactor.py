739c009794b6a31beac994360d1a5a10aafaf93d
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-06 14:43:40 -0700

    up

--- project_euler/largestprimefactor.py
+++ project_euler/largestprimefactor.py
@@ -10,27 +10,27 @@
 
 
 # Function definition to get all prime factors
-def get_prime_factors(n):
-    i = 2
-    prime_factors = []
-    while i*i <= n:
-        if n % i == 0:
-            prime_factors.append(i)
-            n //= i
-        else:
-            i += 1
+# def get_prime_factors(n):
+#     i = 2
+#     prime_factors = []
+#     while i*i <= n:
+#         if n % i == 0:
+#             prime_factors.append(i)
+#             n //= i
+#         else:
+#             i += 1
 
-    if n > 1:
-        prime_factors.append(n)
+#     if n > 1:
+#         prime_factors.append(n)
 
-    return prime_factors
+#     return prime_factors
 
 
-# Read number from user
-number = int(input('Enter number: '))
+# # Read number from user
+# number = int(input('Enter number: '))
 
-# Function call
-prime_factors = get_prime_factors(number)
+# # Function call
+# prime_factors = get_prime_factors(number)
 
-# Displaying prime factors
-print("Prime factors are: ", prime_factors)
+# # Displaying prime factors
+# print("Prime factors are: ", prime_factors)
