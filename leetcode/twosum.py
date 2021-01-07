3c1c6d8ce6f91e20e4c355fb0e2cea689ac39b1d
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-07 12:07:58 -0700

    up

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -2,8 +2,13 @@
 # Difficulty: Easy
 # Leetcode Problem Section
 
+# The program needs to find the index of the values inside the nums list,
+# which can sum together to equalte the value of target
+# three cases defined in this problem which can make it smaller 
+# in compare to other methods
+
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         nums.sort()
         # case 1
-        
+        # 
