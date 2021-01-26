adfc3fa35f7b3e488130984dee02d0301af8d191
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-26 00:49:43 -0700

    up

--- eular/largestprimefactor.py
+++ eular/largestprimefactor.py
@@ -1,12 +1,14 @@
 # Finding Largest Prime Factor
 # The Project Euler Questions
 
+import intertools
+import operator
 
 def prime_factor(number):
     print('The program starts here')
-    
+
 
 
 if __name__ == "__main__":
-    number = input('Enter the number for the factorization')
+    number = input('Enter the number for the factorization: ')
     prime_factor(number)
