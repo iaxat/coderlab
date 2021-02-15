4e149dffc3bc4352bbd8dde054629da23b2a4c98
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-14 23:11:33 -0700

    up

--- leetcode/weekly_contest/1758.py
+++ leetcode/weekly_contest/1758.py
@@ -2,6 +2,19 @@
 
 class Solution:
     def minOperations(self, s: str) -> int:
+        operations = 0
         ar = list(map(lambda x:x, s))
-        # takes string and divides
-        
+        print(ar)
+        for i in len(ar)-1:
+            if ar[i] == ar[i+1]:
+                operations+=1
+                if ar[i+1] == '0':
+                    ar[i+1] = '1'
+                elif ar[i+1] == '1':
+                    ar[i+1] = '0'
+            else:
+                continue
+        return operations
+
+sol = Solution()
+sol.minOperations('0000')
