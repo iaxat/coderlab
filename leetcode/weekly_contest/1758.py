4575778bd08d2dc94ef9c3a9cff8dcb4a744513e
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-14 23:16:30 -0700

    up

--- leetcode/weekly_contest/1758.py
+++ leetcode/weekly_contest/1758.py
@@ -4,17 +4,19 @@ class Solution:
     def minOperations(self, s: str) -> int:
         operations = 0
         ar = list(map(lambda x:x, s))
-        print(ar)
-        for i in len(ar)-1:
-            if ar[i] == ar[i+1]:
-                operations+=1
-                if ar[i+1] == '0':
-                    ar[i+1] = '1'
-                elif ar[i+1] == '1':
-                    ar[i+1] = '0'
-            else:
-                continue
+        n = len(ar)
+        for i in (0,n-1):
+            if i+1<=n-1:
+                if ar[i] == ar[i+1]:
+                    operations+=1
+                    if ar[i+1] == '0':
+                        ar[i+1] = '1'
+                    elif ar[i+1] == '1':
+                        ar[i+1] = '0'
+                else:
+                    continue
+        print(operations)
         return operations
 
 sol = Solution()
-sol.minOperations('0000')
+sol.minOperations('11111110')
