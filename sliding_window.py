6693559ef5b52ab1c345ed15336068dedaa7a9e2
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-17 16:22:44 -0700

    up

--- sliding_window.py
+++ sliding_window.py
@@ -1,5 +1,28 @@
 # Sliding Window
 # Fixed Window
 
-def fixed_window():
+class Sliding():
+    def __init__(self, window_size, arr) -> None:
+        self.window_size = window_size
+        self.arr = arr
+
+    def sliding_window(self):
+
+
+    def find_max(self):
+
     
+    def find_min(self):
+
+
+    def find_sum(self):
+
+
+    def slide_karo(self):
+        print('slide karo bhai')
+
+window_size = 0
+arr = []
+
+slide = Sliding()
+slide.slide_karo()
