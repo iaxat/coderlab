3fdc59c2232f56893694def4d9b95d68e84bbf98
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-17 16:52:18 -0700

    up

--- sliding_window.py
+++ sliding_window.py
@@ -5,21 +5,40 @@ class Sliding():
     def __init__(self, window_size, arr) -> None:
         self.window_size = window_size
         self.arr = arr
+        self.size = len(arr)
 
-    def sliding_window(self):
+    def make_window(self,window_size,arr):
+        self.windows = []
+        for i in self.size:
+            for j in self.size:
+                if j-i+1 == self.window_size:
+                    break
+        self.windows.append(i)
+        self.windows.append(j)
 
+        return self.windows
 
-    def find_max(self):
+    def find_max(self,window_sum_arr):
+        print('find max')
+        max_max = 0
 
+        print('max is ', max_max)
     
-    def find_min(self):
+    def find_min(self,window_sum_arr):
+        print('find min')
 
+    def find_sum(self,make_window):
+        window_sum = 0
+        window_sum_arr = []
 
-    def find_sum(self):
+        
+        return window_sum_arr
 
-
-    def slide_karo(self):
+    def slide_karo(self,windows):
         print('slide karo bhai')
+        make_window = self.make_window(self.window_size,self.arr)
+        sum_array = self.find_sum(make_window)
+        self.find_max(sum_array)
 
 
 
