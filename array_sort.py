14a1d75dbb693d9009c3ed917b160fe78459ebad
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-09 12:23:44 -0700

    up

---
+++ array_sort.py
@@ -0,0 +1,4 @@
+def array_sort():
+    array_sorting = [2, 3, 7, 6, 8, -1, -10, 15]
+    array_sorting = sorted(array_sorting)
+    
--- longest_nice_substring.py
+++ longest_nice_substring.py
@@ -1,6 +1,19 @@
 # 1763. Longest Nice Substring
 # Leetcode
 
-class Solution:
-    def longestNiceSubstring(self, s: str) -> str:
+# class Solution:
+#     def longestNiceSubstring(self, s: str) -> str:
         
+
+# num1 = 1  # Binary value = 01010
+# num2 = 2  # Binary Value = 10100
+
+# print(num1 & num2)  # 00000
+# print(num1 | num2)  # 11110
+# print(num1 ^ num2)  # 11110
+# print(~num1)  # 1111 0101
+# print(num1 << 3)  # 0101 0000
+# print(num2 >> 3)  # 0010
+
+my_string = "0123456789"
+print(my_string[-2:2])
