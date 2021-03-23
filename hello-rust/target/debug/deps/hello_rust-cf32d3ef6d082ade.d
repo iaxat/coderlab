035215c9721738617f7fa80b3269243df21c3dd5
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-03-23 01:59:28 -0600

    up

---
+++ hello-rust/target/debug/deps/hello_rust-cf32d3ef6d082ade.d
@@ -0,0 +1,5 @@
+/home/axat/Desktop/gdrive/github/projectx/hello-rust/target/debug/deps/hello_rust-cf32d3ef6d082ade: src/main.rs
+
+/home/axat/Desktop/gdrive/github/projectx/hello-rust/target/debug/deps/hello_rust-cf32d3ef6d082ade.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/axat/Desktop/gdrive/github/projectx/hello-rust/target/debug/hello-rust: /home/axat/Desktop/gdrive/github/projectx/hello-rust/src/main.rs
