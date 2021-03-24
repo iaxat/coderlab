6bffde73e405e742267d24b3960586a101e35ecd
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-24 13:29:55 -0600

    up

--- data/hello.py
+++ data/hello.py
@@ -7,8 +7,10 @@ def convert_json_numpy():
     with open('EmployeeData.json') as json_file:
         data = json.load(json_file)
         # print(data)
-    print(np.asarray(data))
+    # print(np.asarray(data))
+    data = np.array(data)
 
-    data_set =
+    data_set = tf.data.Dataset.from_tensor_slices(data)
+    print(data_set)
 
 convert_json_numpy()
