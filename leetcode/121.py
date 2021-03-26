96c271861fe8ded8e5c7ca401caa6b4749b41d48
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-26 07:53:47 -0600

    up

--- leetcode/121.py
+++ leetcode/121.py
@@ -1,27 +1,44 @@
 class Solution:
     def maxProfit(self, prices):
-        buy = 0
-        sell = 0
-        profit = []
+        # buy = 0
+        # sell = 0
+        # profit = []
+        # temp = 0
 
-        le = len(prices)
-        for i in range(le):
-            for j in range(le):
-                if i != j:
-                    if (prices[i]<prices[j]) and (prices[j]-prices[i]>=0):
-                        buy = prices[i]
-                        sell = prices[j]
-                        print(buy)
-                        diff = buy-sell
-                        # print(diff)
-                        profit.append(diff)
+        # le = len(prices)
 
+        # for i in range(0,le):
+        #     buy = prices[i]
+        #     for j in range(0,le):
 
-
+        #         if i != j:
+        #             sell = prices[j]
+        #             if (buy<=sell) and (sell-buy>=0) and j>i:
+        #                 # print(buy)
+        #                 diff = sell-buy
+        #                 # print(diff)
+        #                 profit.append(diff)
 
         # print(profit)
-        return profit
+        # return max(profit)
 
-prices = [2,4,1]
+        profit = 0
+
+        le = len(prices)
+        for i in range(0,le):
+            for j in range(0,le):
+                if i!=j and j>i:
+                    buy = prices[i]
+                    sell = prices[j]
+                    if (buy<=sell) and (sell-buy>=0):
+                        diff = sell-buy
+                        if diff>profit:
+                            profit = diff
+
+        print(profit)
+
+
+prices = [7,1,5,3,6,4]
+# 5
 sol = Solution()
 sol.maxProfit(prices)
