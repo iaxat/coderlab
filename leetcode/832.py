9df4f3ed654c31301cca24628cff1aa302e44d6d
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-26 09:15:19 -0600

    up

--- leetcode/832.py
+++ leetcode/832.py
@@ -1,10 +1,12 @@
 class Solution:
     def flipAndInvertImage(self, image):
+        self.image = image
         
+        return image
 
 
 
 
-
+image = [[1,1,0],[1,0,1],[0,0,0]]
 sol = Solution()
 sol.flipAndInvertImage(image)
