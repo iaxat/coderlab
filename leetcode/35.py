116f8d97cac06345fef80377b3ebc6906594c0c1
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-26 06:50:00 -0600

    up

--- leetcode/35.py
+++ leetcode/35.py
@@ -36,8 +36,8 @@ class Solution:
         print(result)
         return result
 
-arr1 = [2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31]
-arr2 = [2,42,38,0,43,21]
+arr1 = [0,1,2,3,1,3,2,4,6,7,9,2,19]
+arr2 = [2,1,4,3,9,6]
 
 sol = Solution()
 sol.relativeSortArray(arr1,arr2)
