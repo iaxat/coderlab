cd801ab0ce8624c6813608e7bb4e12dbddcd3e66
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-26 02:44:15 -0600

    up

--- leetcode/1051.py
+++ leetcode/1051.py
@@ -1,3 +1,10 @@
+# Students are asked to stand in non-decreasing order of heights for an annual photo.
+# Return the minimum number of students that must move in order for all students to be standing in non-decreasing order of height.
+# Notice that when a group of students is selected they can reorder in any possible way between themselves and the non selected students remain on their seats.
+
+# Input: heights = [1,1,4,2,1,3]   Output: 3
+# Input: heights = [5,1,2,3,4]     Output: 5
+
 class Solution:
     def heightChecker(self, heights):
         # method 1
