ea1dfe27ed2bf0749136045b1706e6e0a6463eac
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-26 02:36:08 -0600

    up

---
+++ leetcode/1051.py
@@ -0,0 +1,28 @@
+class Solution:
+    def heightChecker(self, heights):
+
+        # count = 0
+        # check = sorted(heights)
+        # for x,y in zip(heights,check):
+        #     if x != y:
+        #         count += 1
+        # return count
+
+
+        self.heights = heights
+        counter = 0
+        len_heights = len(heights)
+        i = 0
+
+        for i in range(0,len_heights-1):
+            if heights[i] > heights[i+1]:
+                heights[i],heights[i+1] = heights[i+1],heights[i]
+                counter += 1
+            else:
+                pass
+        print(counter)
+        return counter
+
+heights = [5,1,2,3,4]
+start = Solution()
+start.heightChecker(heights)
