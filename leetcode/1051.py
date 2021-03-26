5900c5de32a59636809b4ee0bb44e3274b18310e
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-26 02:54:21 -0600

    up

--- leetcode/1051.py
+++ leetcode/1051.py
@@ -9,26 +9,27 @@
 class Solution:
     def heightChecker(self, heights):
         # method 1
-        # count = 0
-        # check = sorted(heights)
-        # for x,y in zip(heights,check):
-        #     if x != y:
-        #         count += 1
-        # return count
+        count = 0
+        check = sorted(heights)
+        for x,y in zip(heights,check):
+            if x != y:
+                count += 1
+        print(count)
+        return count
 
         # method 2
-        self.heights = heights
-        counter = 0
-        len_heights = len(heights)
+        # self.heights = heights
+        # counter = 0
+        # len_heights = len(heights)
 
-        for i in range(0,len_heights-1):
-            if heights[i] > heights[i+1]:
-                heights[i],heights[i+1] = heights[i+1],heights[i]
-                counter += 1
-            else:
-                pass
-        print(counter)
-        return counter
+        # for i in range(0,len_heights-1):
+        #     if heights[i] > heights[i+1]:
+        #         heights[i],heights[i+1] = heights[i+1],heights[i]
+        #         counter += 1
+        #     else:
+        #         pass
+        # print(counter)
+        # return counter
 
 heights = [5,1,2,3,4]
 start = Solution()
