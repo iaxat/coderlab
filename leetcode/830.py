d0c95661c569caf344f19761da23d78b1e91240b
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-27 04:20:59 -0600

    up

--- leetcode/830.py
+++ leetcode/830.py
@@ -4,33 +4,33 @@ import re
 class Solution:
     def largeGroupPositions(self, s):
         self.s = s
-        result = []
-        output_ = []
-        s = list(s)
-        counter = Counter(s)
+        # result = []
+        # output_ = []
+        # s = list(s)
+        # counter = Counter(s)
         
-        for key in counter:
-            if counter[key] >=3:
-                result.append(key)
-        # s = str(s)
-        # for o in result:
-        #     res = max(re.findall('((?:' + re.escape(o) + ')*)', s),key = len)
-        # print(result)
+        # for key in counter:
+        #     if counter[key] >=3:
+        #         result.append(key)
+        # # s = str(s)
+        # # for o in result:
+        # #     res = max(re.findall('((?:' + re.escape(o) + ')*)', s),key = len)
+        # # print(result)
         
-        if len(result) == 0:
-            return []
-        else:
-            for i in result:
-                final_result = []
-                for j in range(0,len(s)):
-                    if i == s[j]:
-                        final_result.append(int(j))
-                output_.append([final_result[0],final_result[len(final_result)-1]])
+        # if len(result) == 0:
+        #     return []
+        # else:
+        #     for i in result:
+        #         final_result = []
+        #         for j in range(0,len(s)):
+        #             if i == s[j]:
+        #                 final_result.append(int(j))
+        #         output_.append([final_result[0],final_result[len(final_result)-1]])
 
 
 
 
-        print(output_)
+        
 
 s = "abcdddeeeeaabbbcd"
 
