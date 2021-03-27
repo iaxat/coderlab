f8bfeb31635ec217c99efc9a22a0934e0d903e0a
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-27 04:01:44 -0600

    up

--- leetcode/830.py
+++ leetcode/830.py
@@ -1,14 +1,22 @@
 from collections import Counter
+import re
 
 class Solution:
     def largeGroupPositions(self, s):
         self.s = s
         result = []
+        output_ = []
         s = list(s)
         counter = Counter(s)
+        
         for key in counter:
             if counter[key] >=3:
                 result.append(key)
+        # s = str(s)
+        # for o in result:
+        #     res = max(re.findall('((?:' + re.escape(o) + ')*)', s),key = len)
+        # print(result)
+        
         if len(result) == 0:
             return []
         else:
@@ -17,11 +25,14 @@ class Solution:
                 for j in range(0,len(s)):
                     if i == s[j]:
                         final_result.append(int(j))
-                output_ = [[final_result[0],final_result[len(final_result)-1]]]
+                output_.append([final_result[0],final_result[len(final_result)-1]])
+
+
 
 
         print(output_)
 
-s = "abbxxxxzzy"
+s = "abcdddeeeeaabbbcd"
+
 sol = Solution()
 sol.largeGroupPositions(s)
