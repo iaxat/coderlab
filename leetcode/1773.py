49bdf3181f3b1aeef3967a67c01fb5fab2540bac
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-27 07:32:20 -0600

    up

---
+++ leetcode/1773.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countMatches(self, items, ruleKey, ruleValue):
+
+
+
+
+items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]]
+ruleKey = "color"
+ruleValue = "silver"
+sol = Solution()
+sol.countMatches(items, ruleKey, ruleValue)
