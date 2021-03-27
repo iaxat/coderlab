1a4d2af8a090141c191fc64695d07c8fb5729d54
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-27 02:53:31 -0600

    up

--- leetcode/832.py
+++ leetcode/832.py
@@ -4,6 +4,19 @@ class Solution:
         
         
         
+        for i in range(0,len(image)):
+            for j in range(0,len(image[i])):
+                # reverse matrix
+                image[i] = image[i][::-1]
+                
+                if image[i][j] == 0:
+                    image[i][j] = 1
+                    
+                elif image[i][j] == 1:
+                    image[i][j] = 0
+        print(image)
+        return image
+        print(image)
 
 image = [[1,1,0],[1,0,1],[0,0,0]]
 sol = Solution()
