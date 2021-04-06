bbfec88c3cc02937f31c55dee2ab02c021580976
iaxat <55215422+iaxat@users.noreply.github.com>
2021-04-06 04:38:37 -0600

    up

--- leetcode/16.py
+++ leetcode/16.py
@@ -2,9 +2,9 @@ class Solution:
     def threeSumClosest(self, nums, target):
         self.nums = nums
         self.target = target
-
+        
 
 nums = [-1,2,1,-4]
 target = 1
 sol = Solution()
-sol.threeSumClosest()
+sol.threeSumClosest(nums, target)
