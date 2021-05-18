7034827b3e30e81a71d390ea4f9580a46af0ff58
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-18 15:28:10 -0500

    up

--- leetcode/13.py
+++ leetcode/13.py
@@ -1,3 +1,31 @@
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def romanToInt(self, s):
+        result = 0
+        s = list(s)
+        I = 1
+        V = 5
+        X = 10
+        L = 50
+        C = 100
+        D = 500
+        M = 1000
         
+        roman_dict = {I:[V,X], X:[L,C], C:[D,M]}
+        for i in s:
+            
+
+        return result
+
+s = "III"
+sol = Solution()
+sol.romanToInt(s)
