77f0888921019bd7eddb7671ee56c01b0c19d210
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-19 00:05:06 -0500

    up

--- leetcode/7.py
+++ leetcode/7.py
@@ -1,21 +1,25 @@
-
-
-
-
 class Solution:
     def reverse(self, x):
-        x = str(x)
-        if int(x)<0:
-            x = (int(x))*-1
-            x = str(x)
-            x = x[::-1]
-            x = (int(x))*-1
+        if x>=(-2**31) and (x<=2**31-1):
+            if int(x)<0:
+                x = (int(x))*-1
+                x = str(x)
+                x = x[::-1]
+                x = (int(x))*-1
+            else:
+                x = str(x)
+                x = x[::-1]
+                x = int(x)
+            if x>=(-2**31) and (x<=2**31-1):
+                return x
+            else:
+                x = 0
         else:
-            x = x[::-1]
+            x = 0
+        
+        return x
 
-        print(x)
 
-
-x = -123
+x = 1534236469
 sol = Solution()
 sol.reverse(x)
