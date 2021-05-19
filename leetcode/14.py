afe896629dec2ba6c2f06f86b2551c4458367ec8
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-19 00:32:25 -0500

    up

---
+++ leetcode/14.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        for i in strs:
+            
+
+
+strs = ["flower","flow","flight"]
+sol = Solution()
+sol.longestCommonPrefix(strs)
