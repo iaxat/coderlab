54951a2e8518905085eec873b5796f283d478050
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-19 20:02:51 -0500

    up

--- leetcode/14.py
+++ leetcode/14.py
@@ -1,7 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs):
         len_str = len(strs)
-        
+        temp_dict = {}
+        if len_str>0 and len_str<=200:
+            for i in range(0,len_str):
+                temp_len = len(strs[i])
+                temp_dict[temp_len] = strs[i]
+                min_key = min(temp_dict)
+            for j in min_key:
+                
+        print(temp_dict)
 
 
 strs = ["flower","flow","flight"]
