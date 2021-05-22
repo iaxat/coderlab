30b8920dc411ad4e82ef909187a4caa84fe9b433
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-21 22:37:27 -0500

    up

--- leetcode/14.py
+++ leetcode/14.py
@@ -7,7 +7,7 @@ class Solution:
                 temp_len = len(strs[i])
                 temp_dict[temp_len] = strs[i]
                 min_key = min(temp_dict)
-            # for j in min_key:
+            for j in min_key:
                 
 
 
