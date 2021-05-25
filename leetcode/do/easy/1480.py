c00719115c74df8c1c542071860d41231bfebba9
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-25 15:26:29 -0500

    up

--- leetcode/do/easy/1480.py
+++ leetcode/do/easy/1480.py
@@ -2,10 +2,25 @@
 
 class Solution:
     def runningSum(self, nums):
-        
+        new_arr = []
+        counter = 0
+        for i in nums:
+            counter+=i
+            new_arr.append(counter)
 
-
+        print(new_arr)
+        return new_arr
 
 nums = [1,2,3,4]
 sol = Solution()
 sol.runningSum(nums)
+
+
+
+# # method 2
+# class Solution:
+#     def runningSum(self, nums: List[int]) -> List[int]:
+#         for i in range(1,len(nums)):
+#             nums[i] += nums[i-1]
+
+#         return nums
