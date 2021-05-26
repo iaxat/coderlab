d2c5afffcc436ef3c7c746a92f4205448b10b498
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-26 16:19:28 -0500

    up

---
+++ leetcode/do/easy/1389.py
@@ -0,0 +1,12 @@
+# Easy
+
+class Solution:
+    def createTargetArray(self, nums, index):
+        
+
+        
+
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+sol = Solution()
+sol.createTargetArray(nums,index)
