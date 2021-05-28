7c952bbbe6c13649e97de593b8b9dea48f4f468d
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-28 00:16:10 -0500

    up

--- leetcode/do/1588.py
+++ leetcode/do/1588.py
@@ -1,2 +1,21 @@
 # Easy
 
+class Solution:
+    def sumOddLengthSubarrays(self, arr):
+        counter = 0
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                temp_arr = arr[i:j]
+                temp_len = len(temp_arr)
+                if temp_len%2 != 0:
+                    counter+= sum(temp_arr)
+
+        print(counter)
+
+
+
+
+
+arr = [1,4,2,5,3]
+sol = Solution()
+sol.sumOddLengthSubarrays(arr)
