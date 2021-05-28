244e3b508f3656364ec25d8cb140ef3f1e908bd8
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-28 17:41:48 -0500

    up

--- leetcode/do/1534.py
+++ leetcode/do/1534.py
@@ -1,2 +1,28 @@
 # Easy
 
+class Solution:
+    def countGoodTriplets(self, arr: list[int], a: int, b: int, c: int) -> int:
+        len_arr = len(arr)
+        result = []
+        for i in range(len_arr):
+            if i >= 0:
+                for j in range(len_arr):
+                    if i<j:
+                        for k in range(len_arr):
+                            if j<k:
+                                if k<len_arr:
+                                    if arr[i] - arr[j] <= a and arr[j] - arr[k] <= b and arr[i] - arr[k] <= c:
+                                        res_temp = arr[i],arr[j],arr[k]
+                                        res_temp = tuple(res_temp)
+                                        if res_temp not in result:
+                                            result.append(res_temp)
+                                            result_len = len(result)
+        print(result)
+
+
+arr = [3,0,1,1,9,7]
+a = 7
+b = 2
+c = 3
+sol = Solution()
+sol.countGoodTriplets(arr,a,b,c)
---
+++ leetcode/do/1732.py
@@ -0,0 +1,16 @@
+# Easy
+
+class Solution:
+    def largestAltitude(self, gain: list[int]) -> int:
+        self.gain = gain
+        result = 0
+        
+
+
+        print(result)
+        return result
+
+
+gain = [-5,1,5,0,-7]
+sol = Solution()
+sol.largestAltitude(gain)
