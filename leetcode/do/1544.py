c1a0a9ca3c901c61ada349c34bbff2aa5cec081a
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-01 00:01:15 -0500

    up

--- leetcode/do/1544.py
+++ leetcode/do/1544.py
@@ -6,11 +6,9 @@ class Solution:
         result = 0
         for i in range(len(s)-1):
             s = s.replace(s[i],'')
-        
             print(s)
 
 
-    
 s = "leEeetcode"
 sol = Solution()
 sol.makeGood(s)
