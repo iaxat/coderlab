1deee1474796c6d5faa3ff513b9e5d8c17481a7f
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-31 20:23:29 -0500

    up

---
+++ leetcode/done/imp/20.py
@@ -0,0 +1,11 @@
+# Easy
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()','').replace('{}','').replace('[]','')
+        return s == ''
+
+s = "()"
+sol = Solution()
+print(sol.isValid(s))
