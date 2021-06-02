7070aeca1bb7b796215d0fcdc6ca6f67502c4bbd
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-02 01:25:10 -0500

    upi

---
+++ leetcode/do/5.py
@@ -0,0 +1,26 @@
+# Medium
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        # method 1  - time exceeded
+
+        # result = []
+        # final = 0
+        # for i in range(0,len(s)):
+        #     for j in range(1,len(s)+1):
+        #         data = s[i:j]
+        #         if data == data[::-1] and data != '':
+        #             result.append((data))
+        # for k in result:
+        #     if final < len(k):
+        #         final = len(k)
+        #         res = k
+        # return res
+
+        # method 2
+        
+
+s = "babad"
+sol = Solution()
+print(sol.longestPalindrome(s))
