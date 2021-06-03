4eb33ecbd15b2298a0922e32a85e29b7b2eede2c
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-03 00:20:13 -0500

    up

--- sparkwave.py
+++ sparkwave.py
@@ -11,26 +11,36 @@ class Dependent_Libraries():
             lines = f.readlines()
         new_line = []
         for line in lines:
-            new_line.append(line.replace("\n", ""))
-        print(new_line)
+            temp_line = line.replace(" depends on ",",")
+            new_line.append(temp_line.replace("\n", ""))
         return new_line
 
     # Extract the data from the new_line
-    # Which 
+    # Which will return the values
     def extract_data(self,new_line):
+        self.data_dict = data_dict = {}
+        for i in range(len(new_line)):
+            # if i[0] in data_dict.keys():
+            #     data_dict[i[0]].append()
+            # else:
+            #     data_dict[i[]]
+            print(new_line[i])
+
+
+        return data_dict
+
+
+    def create_relation_graph(self):
         print('')
-
-
-    
-    # def create_relation_graph(self):
-        
 
     
     def start_program(self):
         new_line = self.read_file('readme.txt')
-        graph_create = self.extract_data(new_line)
+        data_dict = self.extract_data(new_line)
+        # print(result) = self.create_relation_graph(data_dict)
 
 
 
+# initiate
 sol = Dependent_Libraries()
 print(sol.start_program())
