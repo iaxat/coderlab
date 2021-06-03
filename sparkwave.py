0b20b68351f0d8cab2480241ee566f9b1e6e7ec2
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-03 09:58:00 -0500

    up

--- sparkwave.py
+++ sparkwave.py
@@ -15,32 +15,46 @@ class Dependent_Libraries():
             new_line.append(temp_line.replace("\n", ""))
         return new_line
 
+
     # Extract the data from the new_line
     # Which will return the values
     def extract_data(self,new_line):
         self.data_dict = data_dict = {}
         for i in range(len(new_line)):
+            
             # if i[0] in data_dict.keys():
             #     data_dict[i[0]].append()
             # else:
             #     data_dict[i[]]
+
             print(new_line[i])
-
-
+        print(data_dict)
         return data_dict
 
 
-    def create_relation_graph(self):
-        print('')
+    def create_relation_graph(self,graph,node):
+        visited = [] # List for visited nodes.
+        queue = [] # Initialize a queue
+        visited.append(node)
+        queue.append(node)
+        while queue:# Creating loop to visit each node
+            m = queue.pop(0) 
+            print (m, end = " ") 
+            for neighbour in graph[m]:
+                if neighbour not in visited:
+                    visited.append(neighbour)
+                    queue.append(neighbour)
+        return queue
 
-    
+
     def start_program(self):
         new_line = self.read_file('readme.txt')
         data_dict = self.extract_data(new_line)
-        # print(result) = self.create_relation_graph(data_dict)
+        for i in range(len(data_dict)):
+            result = self.create_relation_graph(data_dict,data_dict[i])
+            print(result)
 
 
-
-# initiate
+# initiation
 sol = Dependent_Libraries()
 print(sol.start_program())
