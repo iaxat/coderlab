bef97eed48cc26fd9bce6ca34f8d76406984ef69
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-02 23:10:23 -0500

    up

--- sparkwave.py
+++ sparkwave.py
@@ -1,15 +1,6 @@
 # Job Application Coding Assignment
-# 
 
 # Funtions implemented in the file
-
-# 1 . Read File
-# Dict create
-
-# DFS? pros: cons:
-# BFS? pros: cons:
-
-
 class Dependent_Libraries():
     def __init__(self) -> None:
         pass
@@ -18,15 +9,28 @@ class Dependent_Libraries():
     def read_file(self,file):
         with open(file) as f:
             lines = f.readlines()
+        new_line = []
+        for line in lines:
+            new_line.append(line.replace("\n", ""))
+        print(new_line)
+        return new_line
 
-        return lines
+    # Extract the data from the new_line
+    # Which 
+    def extract_data(self,new_line):
+        print('')
 
-    def extract_data(self,lines):
+
+    
+    # def create_relation_graph(self):
         
 
-
     
-    def create_relation_graph(self):
+    def start_program(self):
+        new_line = self.read_file('readme.txt')
+        graph_create = self.extract_data(new_line)
 
 
-    
+
+sol = Dependent_Libraries()
+print(sol.start_program())
