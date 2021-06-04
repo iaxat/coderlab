853334d0afaee4bde9107148e89d34599caa236d
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-04 17:29:19 -0500

    up

--- leetcode/done/imp/1.py
+++ leetcode/done/imp/1.py
@@ -15,29 +15,36 @@ class Solution:
         # print(result)
 
         # method 2
-        # using the target subtraction
-        result = []
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i<j and target-nums[i]==nums[j]:
-                    result = [i,j]
-        
-        # return result
-        # print(result)
-
-
-        # method 3
         hash_table = {}
         for i in range(len(nums)):
             if nums[i] in hash_table:
-                print([hash_table[nums[i]], i])
+                return([hash_table[nums[i]], i])
             else:
                 hash_table[target - nums[i]] = i
 
 
+        # method 3
+        nums_hash = {}
+        for i, num in enumerate(nums):
+            potentialMatch = target - num
+            if potentialMatch in nums_hash:
+                print([nums_hash[potentialMatch], i])
+            nums_hash[num] = i
+
 
         # method 4
-        
+        for idx, val in enumerate(nums):
+                if target - val in nums[idx + 1:]:
+                    return [idx, nums[idx + 1:].index(target - val) + (idx + 1)]
+
+        # method 5
+        cache = {}
+        for idx, n in enumerate(nums):
+            cv = cache.get(target - n)
+            if cv != None and cv != idx:
+                return [cv, idx]
+            cache[n] = idx
+        return []
 
 
 nums = [2,7,11,15]
