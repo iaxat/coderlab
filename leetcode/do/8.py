9398c4a1358f4c9f9f6f739b2c396cf1e52c8198
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-08 02:11:21 -0500

    up

--- leetcode/do/8.py
+++ leetcode/do/8.py
@@ -5,31 +5,43 @@ class Solution:
         results = 0
         stacks = []
         temp_arr = ''
-        if s[0].isnumeric() == True:
-            for i in range(len(s)):
-                    if s[i] == ' ' and len(temp_arr)>=1:
-                        stacks.append(temp_arr)
-                        temp_arr = ''
-                        continue
 
-                    if s[i] == ' ' and len(temp_arr)<1:
-                        continue
+        new_arr = s.split(' ')
+        print(new_arr)
+        for i in range(len(s)):
+            if s[i] == ' ' and len(temp_arr)>=1:
+                stacks.append(temp_arr)
+                temp_arr = ''
+                continue
+            
+            if s[i] == ' ' and len(temp_arr)<1:
+                continue
 
-                    if s[i] == '-':
-                        temp_arr += s[i]
+            if s[i] == ' ':
+                pass
 
-                    if s[i].isnumeric():
-                        temp_arr += s[i]
+            if s[i] == '-':
+                temp_arr += s[i]
+
+            if s[i].isnumeric():
+                temp_arr += s[i]
     
         stacks.append((temp_arr))
         results = max(stacks)
-    
-        # if results<-2**31:
-        #     return -2**31
-        # elif results>2**31-1:
-        #     return 2**31-1
+        if results == '':
+            results = 0
+        else:
+            results = int(results)
+
+        if results<-2**31:
+            return -2**31
+        elif results>2**31-1:
+            return 2**31-1
+
+        print(results)
         return results
 
-s = "words and 987"
+# "words and 987"
+s = "   -42 words and 987"
 sol = Solution()
 sol.myAtoi(s)
