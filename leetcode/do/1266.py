edcb663277f4a1e099c3b5c8c1f871cf09a4c6f1
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-11 12:24:38 -0500

    up

--- leetcode/do/1266.py
+++ leetcode/do/1266.py
@@ -3,10 +3,10 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points):
         counter = 0
-        for i in range(len(points)):
-            if 
+
                 
 
 points = [[1,1],[3,4],[-1,0]]
 sol = Solution()
 print(sol.minTimeToVisitAllPoints(points))
+ 
