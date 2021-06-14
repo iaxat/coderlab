992743536fed21e5fd906fe1d9449bc4884b0239
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-14 15:47:55 -0500

    up

--- daily_coding_challenge/14june.py
+++ daily_coding_challenge/14june.py
@@ -3,18 +3,24 @@
 
 class Solution():
     def analysis(self,nums):
-        nums = sorted(nums)
-        for i in range(len(nums)):
-            if nums[i] > 0:
+        results = []
+        nums = sorted(nums,reverse=True)
+        print(nums)
+        for i in range(len(nums)-1):
+            if nums[i] > 0 and nums[i+1]>0:
                 # case 1
-                
+                if nums[i] - nums[i+1] > 1:
+                    results.append(nums[i+1]+1)
 
-                # case 2
+        # case 2
+        if len(results) == 0:
+            if nums[0]>0:
+                results.append(nums[0]+1)
+
+        return min(results)
 
 
-
-
-nums = [3, 4, -1, 1]
+nums = [1, 2, 0]
 # [1, 2, 0]
 sol = Solution()
-sol.analysis(nums)
+print(sol.analysis(nums))
