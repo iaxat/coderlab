56bf10a61fe65d293b69764714f32d7d426982f6
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-14 15:26:54 -0500

    up

--- daily_coding_challenge/13june.py
+++ daily_coding_challenge/13june.py
@@ -6,7 +6,7 @@ class Node:
         self.left = left
         self.right = right
 
-
+    
 
 
 
@@ -23,7 +23,7 @@ class Node:
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# The following test should pass:
 
+# The following test should pass:
 # node = Node('root', Node('left', Node('left.left')), Node('right'))
 # assert deserialize(serialize(node)).left.left.val == 'left.left'
---
+++ daily_coding_challenge/14june.py
@@ -0,0 +1,20 @@
+# Hard
+
+
+class Solution():
+    def analysis(self,nums):
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                # case 1
+                
+
+                # case 2
+
+
+
+
+nums = [3, 4, -1, 1]
+# [1, 2, 0]
+sol = Solution()
+sol.analysis(nums)
