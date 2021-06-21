ec88b2d4b9e04bcb95c13c676a0cd4f0333ee538
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-21 01:05:17 -0500

    up

---
+++ swift_exp/py/Shared/pyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pyApp.swift
+//  Shared
+//
+//  Created by Kumar Akshat on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct pyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
