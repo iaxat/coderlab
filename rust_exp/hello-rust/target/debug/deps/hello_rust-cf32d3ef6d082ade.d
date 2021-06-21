d24b0416d5ad9aeda2b14c8ad4cf3e7fa0c4ae6f
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-21 01:05:26 -0500

    up

--- rust_exp/hello-rust/target/debug/deps/hello_rust-cf32d3ef6d082ade.d
+++
@@ -1,5 +0,0 @@
-/home/axat/Desktop/gdrive/github/projectx/hello-rust/target/debug/deps/hello_rust-cf32d3ef6d082ade: src/main.rs
-
-/home/axat/Desktop/gdrive/github/projectx/hello-rust/target/debug/deps/hello_rust-cf32d3ef6d082ade.d: src/main.rs
-
-src/main.rs:
--- rust_exp/hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/axat/Desktop/gdrive/github/projectx/hello-rust/target/debug/hello-rust: /home/axat/Desktop/gdrive/github/projectx/hello-rust/src/main.rs
