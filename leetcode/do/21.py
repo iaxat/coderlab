a998f31554869e540734bff72bc47c5c3aceafe9
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-25 12:32:51 -0500

    up

--- leetcode/do/21.py
+++ leetcode/do/21.py
@@ -1,19 +1,27 @@
 # # Easy
 
 # # Definition for singly-linked list.
-class LIST_:
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 class Solution:
-    def mergeTwoLists(self, l1: LIST_, l2: LIST_) -> LIST_:
-    	l1.next = l2.val
-
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
 l1 = [1,2,4], l2 = [1,3,4]
 sol = Solution()
-sol.mergeTwoLists(l1,l2)
+print(sol.mergeTwoLists(l1,l2))
 
 
 # # Node class
