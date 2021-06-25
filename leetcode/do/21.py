756f119553e0b37e71bd5906be16287c42bd748d
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-25 01:02:48 -0500

    up

---
+++ leetcode/do/21.py
@@ -0,0 +1,16 @@
+# Easy
+
+# Definition for singly-linked list.
+class LIST_:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: LIST_, l2: LIST_) -> LIST_:
+        while 
+
+
+l1 = [1,2,4], l2 = [1,3,4]
+sol = Solution()
+sol.mergeTwoLists(l1,l2)
