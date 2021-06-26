bc4f22576208f563dfba21d7cd9573719cc0aa28
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-26 18:15:16 -0500

    up

--- leetcode/do/1874.py
+++ leetcode/do/1874.py
@@ -3,9 +3,26 @@
 class Solution:
     def minProductSum(self, nums1: list[int], nums2: list[int]) -> int:
         
+        # method 1 - time limit exceed
+        # results = 0
+        # for i in range(len(nums1)):
+        #     a = max(nums1)
+        #     b = min(nums2)
+        #     results += a * b
+        #     nums1.pop(nums1.index(a))
+        #     nums2.pop(nums2.index(b))
+        # return results
+
+
+        # method 2 - recursion
+        results = 0
+        if len(nums1) == 0:
+            return results
+        
 
 
 
-
+nums1 = [5,3,4,2]
+nums2 = [4,2,2,5]
 sol = Solution()
-sol.minProductSum(nums1,num2)
+print(sol.minProductSum(nums1,nums2))
