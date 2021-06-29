e95e571587b04faf6fc9cdf2985eefc811a73753
iaxat <55215422+iaxat@users.noreply.github.com>
2021-06-29 13:20:05 -0500

    up

---
+++ daily_coding_challenge/28june.py
@@ -0,0 +1,28 @@
+# Hard
+
+class Solution():
+    def __init__(self, k, arr_) -> None:
+        self.k = k
+        self.arr_ = arr_
+    
+    def do_it(self):
+        # using the sliding window approach
+        
+
+        
+        
+        # using the recursion approach
+
+
+        
+        
+        # using the loop approach
+
+
+
+
+
+k = 3
+arr_ = [10, 5, 2, 7, 8, 7]
+sol = Solution(k, arr_)
+sol.do_it()
