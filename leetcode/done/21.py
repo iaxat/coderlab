934157fc7fb92df6d8a71b0f080305c3ac85520b
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-12 19:26:30 -0500

    up

--- leetcode/done/21.py
+++ leetcode/done/21.py
@@ -31,14 +31,15 @@ class Solution:
     def mergeList_recursion(self, l1:ListNode, l2:ListNode) -> ListNode:
         if l1 is None:
             return l2
-        if l2 is None:
+        elif l2 is None:
             return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
         
-
-
-
-
-
 l1 = [1,2,4]
 l2 = [1,3,4]
 sol = Solution()
@@ -50,8 +51,9 @@ def create_linkedList(data):
         a = ListNode(i)
         prev.next=a
         prev = prev.next
-
     return list_head.next
 
-sol.mergeTwoLists(create_linkedList(l1),create_linkedList(l2))
-sol.mergeList_recursion(create_linkedList(l1),create_linkedList(l2))
+# sol.mergeTwoLists(create_linkedList(l1),create_linkedList(l2))
+data = print(sol.mergeList_recursion(create_linkedList(l1),create_linkedList(l2)))
+for i in data:
+    print(i.next.val)
