809348ac8781f137d33aa139cc28a597e9589a7f
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-13 19:22:09 -0500

    up

--- leetcode/do/8.py
+++ leetcode/do/8.py
@@ -4,12 +4,11 @@ class Solution:
     def myAtoi(self, s: str) -> int:
         results = 0
         s = ''.join(s.split())
-        if s.isalpha() or s[0].isalpha():
-            return results
-        temp_arr = []
-        for i in range(0,len(s)):
+        print(s)
+        if s[0].
+            print('yes')
 
 
-s = "      --2322 42asdasd    asdasda   -12312"
+s = "      -2322 42asdasd    asdasda   -12312"
 sol = Solution()
 print(sol.myAtoi(s))
---
+++ leetcode/do/demo.py
@@ -0,0 +1,3 @@
+# enque and deque
+
+
