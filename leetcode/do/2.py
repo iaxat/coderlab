eebd4d56e5379e7f1b9ee09654950b231a8e79ce
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-15 21:43:47 -0500

    up

--- leetcode/do/2.py
+++ leetcode/do/2.py
@@ -6,18 +6,38 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        prehead = ListNode(-1)
-        prev = prehead
-        count_extra = 0
-        if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
-        while l1 and l2:
-            
+        a = ''.join(l1)
+        b = ''.join(l2)
+        result = a+b
+        
+        
+        
+        
+        
+        # prehead = ListNode(-1)
+        # val_count = ListNode(0)
+        # prev = prehead
+        # if l1 is None:
+        #     return l2
+        # if l2 is None:
+        #     return l1
+        # while l1 and l2:
+        #     # count_extra = 0
+        #     val_count.val = l1.val + l2.val
+        #     prev.next = val_count
+        #     l1 = l1.next
+        #     l2 = l2.next
+        #     prev = prev.next
 
+        # prev.next = l1 if l1 is not None else l2
+
+        # while prehead.next is not None:
+        #     prehead = prehead.next
+        #     print(prehead.val)
+        # return prehead.next
 
 
 l1 = [2,4,3]
@@ -33,4 +53,4 @@ def create_linkedList(data):
     return list_head.next
 
 sol = Solution()
-sol.addTwoNumbers(create_linkedList(l1), create_linkedList(l2))
+sol.addTwoNumbers(l1, l2)
