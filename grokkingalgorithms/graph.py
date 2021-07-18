6c914fad9058bd296dc817e590dcebc933eea1b4
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-18 14:43:55 -0500

    up

---
+++ grokkingalgorithms/graph.py
@@ -0,0 +1,40 @@
+# [a,b],[b,c],[c,d]
+
+class Create():
+    def __init__(self, data) -> None:
+        self.data = data
+
+    def create_graph_directed(self) -> dict:
+        graph_dict = {}
+        for i in range(0,len(self.data)):
+            for j in range(1,len(self.data[i])):
+                key_dict = self.data[i][0]
+                values_data = self.data[i][j]
+                if key_dict not in graph_dict.keys():
+                    graph_dict[key_dict] = [values_data]
+                else:
+                    graph_dict[key_dict].append(values_data)
+        return graph_dict
+
+
+    def create_graph_undirected(self) -> dict:
+        graph_dict = {}
+        for i in range(0,len(self.data)):
+            a = 0
+            key_dict = self.data[i][a]
+            for j in range(0,len(self.data[i])):
+                values_data = self.data[i][j]
+                if key_dict not in graph_dict.keys() and key_dict != values_data:
+                    graph_dict[key_dict] = [values_data]
+                elif key_dict in graph_dict.keys() and key_dict != values_data:
+                    graph_dict[key_dict].append(values_data)
+            a += 1
+
+        return graph_dict
+
+
+data = [['a','b'],['a','c'],['c','d']]
+sol = Create(data)
+print(sol.create_graph_directed())
+print(sol.create_graph_undirected())
+# {a=[b, c], b=[a], c=[a, d], d=[c]}
--- leetcode/do/demo.py
+++ leetcode/do/demo.py
@@ -1,7 +1,18 @@
-def reverse(input): 
-    output = ""
-    for index in range(len(input), 0):  
-        output += input[index-1]
-    print(output)
+import json
 
-reverse('Hello')
+def addStrawberry():
+    with open('fake_fruit.json', 'r') as f:   
+        data = json.load(f)                   
+        dict0 = {'Name': 'Strawberry', 'Colour': 'Red'}
+        data['Fruit'].append(dict0)
+
+    with open ('fake_fruit.json', 'w') as f:
+        json.dump(data, f, indent=4)        
+
+    with open('fake_fruit.json', 'r') as f:
+        print(f.read()) 
+
+    print(data)
+    # return data
+
+addStrawberry()
