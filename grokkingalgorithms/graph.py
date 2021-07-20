f9c466c1c87ac943e25adbfd067f37c5c449db45
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-20 05:29:44 -0500

    up

--- grokkingalgorithms/graph.py
+++ grokkingalgorithms/graph.py
@@ -1,51 +1,79 @@
 # [a,b],[b,c],[c,d]
 
-from typing import get_args
-
-
 class Create():
     def __init__(self, data) -> None:
         self.data = data
 
     def create_graph_directed(self) -> dict:
+        # graph_dict = {}
+        # for i in range(0,len(self.data)):
+        #     for j in range(1,len(self.data[i])):
+        #         key_dict = self.data[i][0]
+        #         values_data = self.data[i][j]
+        #         if key_dict not in graph_dict.keys():
+        #             if values_data is not None:
+        #                 graph_dict[key_dict] = [values_data]
+        #             else:
+        #                 graph_dict[key_dict] = []
+        #         else:
+        #             if values_data is not None:
+        #                 graph_dict[key_dict].append(values_data)
+        #             else:
+        #                 graph_dict[key_dict] = []
+                    
+        # return graph_dict
+
+        # 1 for loop
         graph_dict = {}
-        for i in range(0,len(self.data)):
-            for j in range(1,len(self.data[i])):
-                key_dict = self.data[i][0]
-                values_data = self.data[i][j]
-                if key_dict not in graph_dict.keys():
-                    graph_dict[key_dict] = [values_data]
-                else:
-                    graph_dict[key_dict].append(values_data)
+        graph_dict = {}
+        for edge in self.data:
+            if edge[0] not in graph_dict.keys():
+                graph_dict[edge[0]] = [edge[1]]
+            else:
+                graph_dict[edge[0]].append(edge[1])
+            if edge[1] not in graph_dict.keys():
+                graph_dict[edge[1]] = []
+
         return graph_dict
 
 
     def create_graph_undirected(self) -> dict:
         graph_dict = {}
         for edge in self.data:
-            print(edge[0])
             if edge[0] not in graph_dict.keys():
-                graph_dict[edge[0]] = []
+                graph_dict[edge[0]] = [edge[1]]
             else:
-                graph_dict[edge[0]] = edge[1]
+                graph_dict[edge[0]].append(edge[1])
+            if edge[1] not in graph_dict.keys():
+                graph_dict[edge[1]] = [edge[0]]
+            else:
+                graph_dict[edge[1]].append(edge[0])
         return graph_dict
 
-        # for i in range(0,len(self.data)):
-        #     a = 0
-        #     key_dict = self.data[i][a]
-        #     for j in range(0,len(self.data[i])):
-        #         values_data = self.data[i][j]
-        #         if key_dict not in graph_dict.keys() and key_dict != values_data:
-        #             graph_dict[key_dict] = [values_data]
-        #         elif key_dict in graph_dict.keys() and key_dict != values_data:
-        #             graph_dict[key_dict].append(values_data)
-        #     a += 1
 
-        return graph_dict
+    def dfs_graph_directed(self):
+        graph_dict_data = self.create_graph_directed()
+        # stack = ['a']
+        stack = []
+        for node in graph_dict_data:
+            stack.append(node)
+        visited = set()
+        while stack:
+            current = stack.pop()
+            if current not in visited:
+                for child in graph_dict_data[current]:
+                    if child not in visited:
+                        stack.append(child)
+                visited.add(current)
+        print(visited)
 
 
 data = [['a','b'],['a','c'],['c','d']]
 sol = Create(data)
-# print(sol.create_graph_directed())
+print(sol.create_graph_directed())
 print(sol.create_graph_undirected())
+sol.dfs_graph_directed()
 # {a=[b, c], b=[a], c=[a, d], d=[c]}
+
+# detect cycle in graph
+
