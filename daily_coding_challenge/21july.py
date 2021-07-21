db2f94e109391301e5dee05d4727faff3cce0381
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-21 12:26:36 -0500

    up

--- daily_coding_challenge/21july.py
+++ daily_coding_challenge/21july.py
@@ -1,13 +1,20 @@
 # Medium
 
 class Route_Finder():
-    def __init__(self, path) -> None:
+    def __init__(self, path, start_point) -> None:
         self.path = path
+        self.start_point = start_point
 
     def final_route(self):
+        print('')
         
 
 
 
+
 path = [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')]
-sol = Route_Finder(path)
+#  ['A', 'B', 'C', 'A', 'C']
+# ['A', 'C', 'A', 'B', 'C']
+start_point = 'A'
+sol = Route_Finder(path, start_point)
+print(sol.final_route())
