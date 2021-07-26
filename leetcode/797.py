e7d395ef63ca615ed4580fe4c50baa1135e99dea
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-25 22:34:09 -0500

    up

--- leetcode/797.py
+++ leetcode/797.py
@@ -3,17 +3,11 @@
 class Solution():
     def allPathsSourceTarget(self, graph: list[list[int]]) -> list[list[int]]:
         graph_dict = {}
-        for i in range(0,len(graph)):
+        for i in graph:
             graph_dict[i] = graph[i]
-        print(graph_dict)
-        results = []
-        stacks = [0]
-        while stacks:
-            temp_res = []
-            for i in graph_dict.keys():
                 
 
-                
+
 
 graph = [[1,2],[3],[3],[]]
 sol = Solution()
