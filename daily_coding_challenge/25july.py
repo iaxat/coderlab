145fc72b553eb8069e8cc87107f5aed596176f5a
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-26 19:13:18 -0500

    up

---
+++ daily_coding_challenge/25july.py
@@ -0,0 +1,16 @@
+# Easy
+import math
+
+class Solution():
+    def __init__(self, rand5, rand7) -> None:
+        self.rand5 = rand5
+        self.rand7 = rand7
+
+    def find_sol(self):
+        
+
+
+rand5 = 5
+rand7 = 7
+sol = Solution(rand5, rand7)
+sol.find_sol()
