824241b89b73279739f8b7891926bd25c039d2e5
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-30 14:56:33 -0500

    up

--- leetcode/1165.py
+++ leetcode/1165.py
@@ -1,15 +1,24 @@
 # Easy
 
 class Solution:
-    def calculateTime(self, keyboard: str, word: str) -> int:
+    def calculateTime(self, keyboard: str, word: str) -> int:        
         start = end = 0
-        
+        time = 0
         for i in word:
-            
+            end = keyboard.index(i)
+            time += abs(start - end)
+            start = end
+        
+        return time
+
+
+    # method 2
+    def calculateTime2(self, keyboard, word):
+        
 
 
 
-keyboard = "abcdefghijklmnopqrstuvwxyz"
-word = "cba"
+keyboard = "pqrstuvwxyzabcdefghijklmno"
+word = "leetcode"
 sol = Solution()
 sol.calculateTime(keyboard, word)
