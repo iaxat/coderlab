d820383124de701cd8da5d77b277ec0de197b775
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-30 13:27:44 -0500

    up

---
+++ leetcode/1165.py
@@ -0,0 +1,15 @@
+# Easy
+
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        start = end = 0
+        
+        for i in word:
+            
+
+
+
+keyboard = "abcdefghijklmnopqrstuvwxyz"
+word = "cba"
+sol = Solution()
+sol.calculateTime(keyboard, word)
--- leetcode/3.py
+++ leetcode/3.py
@@ -2,16 +2,19 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        stacks = []
+        # sliding window
+        sliding_window = []
         counter = 0
-        for i in range(len(s)):
-            
-            while s[i] not in stacks:
-                stacks.append(s[i])
-            
+        for i in range(0,len(s)):
+            for j in range(i+1,len(s)):
+                sliding_window = s[i:j]
+                if len(set(sliding_window)) == len(sliding_window):
+                    
+                    if len(sliding_window) > counter:
+                        counter = len(sliding_window)
+        print(counter)
 
-        return len(stacks)
 
-s = "pwwkew"
+s = "dvdf"
 sol = Solution()
 print(sol.lengthOfLongestSubstring(s))
